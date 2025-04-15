eaf57d173bfa259642d4f5c56f61b91319ccab3e
yuvaarajj <ryuva125@gmail.com>
2025-04-15 13:30:00 +0530

    Done with Landing page and details section

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,7 @@ function Header() {
 <h1 className='text-5xl md:text-7xl font-extrabold text-transparent bg-clip-text 
 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 
 hover:from-yellow-400 hover:via-red-500 hover:to-pink-500 
-transition-all duration-700 ease-in-out cursor-pointer'>AI Trip Planner</h1>
+transition-all duration-700 ease-in-out cursor-pointer'>AI Trip Planner (DEMO)</h1>
       <Button variant='contained'>Sign in</Button>
     </div>
   )
